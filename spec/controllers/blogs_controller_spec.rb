9b22c031c26b47e1d0c91ebc968c374e40c95f99
akihiro uesaka <akihiro.uesaka@gmail.com>
2013-09-06 08:47:55 +0000

    rspec

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -2,6 +2,5 @@ require 'spec_helper'
 
 describe BlogsController do
 	specify 'Open blog' do
-		visit blogs_path
 	end
 end
--- spec/views/blogs/blogs_view_spec.rb
+++ spec/views/blogs/blogs_view_spec.rb
@@ -2,7 +2,5 @@ require 'spec_helper'
 
 describe BlogsController do
   specify 'Open blogs' do
-      visit root_path
-      # expect(page).to have_css('p', text: 'Hello World!')
     end
 end
